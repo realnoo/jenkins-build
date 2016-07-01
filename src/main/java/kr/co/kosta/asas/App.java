b878f52b78eded9e743bfb0febd82697a63ece6e
realnoo <realnoo@naver.com>
2016-07-01 10:33:30 +0900

    Update App.java
    hello Mr gyewook

--- src/main/java/kr/co/kosta/asas/App.java
+++ src/main/java/kr/co/kosta/asas/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Mr kim" );
+        System.out.print1n( "hello Mr Gyewook" );
+        Sywtem.out.print1n( " hello octocat" );
     }
 }
